849f41b951544b7851c9776851ef8b4caa6dda40
Kislov Dmitry <kislov@easydan.com>
2016-09-05 14:07:08 +1000

    Compile process fixed

--- compile.py
+++ compile.py
@@ -13,15 +13,20 @@ with open('maintemplate.tex') as f:
     maintemplate = f.read()
 template = Template(unicode(maintemplate.decode('utf-8')))
 
+toeval = []
 for root, dirs, files in os.walk("."):
     for ff in files:
         if lecture_pat.match(root):
             if 'lecture' in ff:
-                with open(os.path.join(root, ff), 'r') as f:
-                    texdata = f.read()
-                with open(os.path.join(root, ff+'.tex'), 'w') as f:
-                    f.write(template.render(content=texdata.decode('utf-8')).encode('utf-8'))
-                os.chdir(root)
-                subprocess.call(['pdflatex', ff+'.tex'])
-                subprocess.call(['pdflatex', ff+'.tex'])
-         
+                if '.' not in ff:
+                    with open(os.path.join(root, ff), 'r') as f:
+                        texdata = f.read()
+                    with open(os.path.join(root, ff+'.tex'), 'w') as f:
+                        f.write(template.render(content=texdata.decode('utf-8')).encode('utf-8'))
+                    toeval.append(os.path.join(root, ff+'.tex'))
+                
+for lect in toeval:
+    pp, ff = os.path.split(lect)
+    os.chdir(pp)
+    subprocess.call(['pdflatex', ff])
+    subprocess.call(['pdflatex', ff])
