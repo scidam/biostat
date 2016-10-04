8906070ef75ad28fc9809483ac81d2ead5a7a269
Dmitry E.Kislov <kislov@easydan.com>
2016-10-04 19:43:20 +1000

    2 problems to Lect3

--- Lecture3/tasks/gen.py
+++ Lecture3/tasks/gen.py
@@ -5,19 +5,42 @@ import numpy as np
 
 
 #---------- Sparse graph generation algo ----------
-x = np.random.rand(100,100)
-
-x[x>0.99] = 1
-x[x<=0.99] = 0
-x = x.astype(int)
-print x.shape
-np.savetxt('out.dat',x, fmt='%d')
+# x = np.random.rand(100,100)
+# 
+# x[x>0.99] = 1
+# x[x<=0.99] = 0
+# x = x.astype(int)
+# print x.shape
+# np.savetxt('out.dat',x, fmt='%d')
 # -------------------------------------------------
 
 
 
 # gathered from vbgi collections... 
 common_species = [
-    ''
-    
+    'Pseuderanthemum atropurpureum',
+    'Strobilanthes isophyllus',
+    'Agave stricta',
+    'Onychium japonicum',
+    'Agave filifera',
+    'Hosta rectifolia',
+    'Hosta rectifolia',
+    'Lithops aucampiae',
+    'Caldesia reniformis',
+    'Allium schoenoprasum',
+    'Allium spirale',
+    'Aloe zebrina',
+    'Aloe aristata',
+    'Plumeria rubra'
     ]
+
+
+res = [[],[]]
+
+for k in range(100):
+    res[0].append(np.random.choice(common_species[2:]))
+    res[1].append(np.random.choice(common_species[:-2]))
+    
+np.savetxt('output.dat', np.array(res).T, fmt='%s', delimiter=';')    
+    
+
