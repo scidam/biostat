dee37913499fbcfa554c07e877ca1690822ee583
Dmitry E.Kislov <kislov@easydan.com>
2016-10-04 18:24:38 +1000

    Graph problems added

---
+++ Lecture3/tasks/gen.py
@@ -0,0 +1,15 @@
+
+from itertools import combinations
+
+import numpy as np 
+
+
+#---------- Sparse graph generation algo ----------
+x = np.random.rand(100,100)
+
+x[x>0.99] = 1
+x[x<=0.99] = 0
+x = x.astype(int)
+print x.shape
+np.savetxt('out.dat',x, fmt='%d')
+# -------------------------------------------------
